d7e1d0a41b2040710cc605fadbe22a3907d0af48
aghabalaguluzade <agabala.oyunda@gmail.com>
2024-01-28 21:37:29 +0400

    Create profile page

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -4,9 +4,9 @@ import { onMounted, ref } from 'vue';
 const model = defineModel({
     modelValue : {
         type: String,
-        required: true
+        required: true,
     },
-    placeholder : String
+    placeholder: String
 });
 
 const input = ref(null);
@@ -25,6 +25,6 @@ defineExpose({ focus: () => input.value.focus() });
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
         v-model="model"
         ref="input"
-        :placeholder="placeholder"
-    />
+        placeholder="placeholder"    
+        />
 </template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({
     canLogin: {
@@ -12,32 +13,34 @@ defineProps({
     canRegister: {
         type: Boolean,
     },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
+    // laravelVersion: {
+    //     type: String,
+    //     required: true,
+    // },
+    // phpVersion: {
+    //     type: String,
+    //     required: true,
+    // },
 });
 </script>
 
 <template>
     <Head title="Social Media Website" />
 
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList/>
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList/>
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <CreatePost/>
+                <PostList class="flex-1"/>
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList />
-        </div>
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <CreatePost />
-            <PostList class="flex-1"/>
-        </div>
-    </div>
+    </AuthenticatedLayout>
 
 </template>
 
