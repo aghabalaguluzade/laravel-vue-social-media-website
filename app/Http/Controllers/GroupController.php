1644be65b07cb367ae8733591b434898c212891a
aghabalaguluzade <aghabalaguluzade@gmail.com>
2024-05-09 00:03:05 +0400

    implement create group

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Enums\GroupUserRole;
+use App\Http\Enums\GroupUserStatus;
+use App\Http\Requests\StoreGroupRequest;
+use App\Http\Requests\UpdateGroupRequest;
+use App\Http\Resources\GroupResource;
+use App\Models\Group;
+use App\Models\GroupUser;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(StoreGroupRequest $request)
+    {
+        $data = $request->validated();
+        $data['user_id'] = auth()->id();
+        $group = Group::create($data);
+
+        $groupUserData = [
+            'status' => GroupUserStatus::APPROVED->value,
+            'role' => GroupUserRole::ADMIN->value,
+            'user_id' => auth()->id(),
+            'group_id' => $group->id,
+            'created_by' => auth()->id()
+        ];
+
+        GroupUser::create($groupUserData);
+
+        return response(new GroupResource($group), 201);
+    }
+
+    public function show(Group $group)
+    {
+
+    }
+
+    public function update(UpdateGroupRequest $request, Group $group)
+    {
+
+    }
+
+    public function destroy(Group $group)
+    {
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\GroupController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,19 +33,27 @@ Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified
 Route::get('/u/{user:username}', [ProfileController::class, 'index'])->name('profile');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::post('/profile/update-images', [ProfileController::class, 'updateImages'])->name('profile.updateImages');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Posts
     Route::post('/posts', [PostController::class, 'store'])->name('post.store');
     Route::put('/posts/{post}', [PostController::class, 'update'])->name('post.update');
     Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.destroy');
     Route::get('/post/download/{attachment}', [PostController::class, 'downloadAttachment'])->name('post.download');
     Route::post('/post/{post}/reaction', [PostController::class, 'postReaction'])->name('post.reaction');
+
+    // Comments
     Route::post('/post/{post}/comment', [PostController::class, 'createComment'])->name('post.comment.create');
     Route::delete('/comment/{comment}', [PostController::class, 'deleteComment'])->name('post.comment.delete');
     Route::put('/comment/{comment}', [PostController::class, 'updateComment'])->name('post.comment.update');
     Route::post('/comment/{comment}/reaction', [PostController::class, 'commentReaction'])->name('post.comment.reaction');
+
+    // Groups
+    Route::post('/group', [GroupController::class, 'store'])->name('group.create');
+
 });
 
 require __DIR__.'/auth.php';
